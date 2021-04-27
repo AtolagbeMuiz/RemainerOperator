becb295a88ee5931f188708e0fec153161c0e76d
Atolagbe Muiz <atolagbemuiz@gmail.com>
2021-04-27 05:22:07 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LabAssignmentWeek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Takes the First Number
+           System.Console.WriteLine(" Welcome... What Number will you like to Divide \n Enter the First Number");
+           var firstNumber = int.Parse(Console.ReadLine());
+
+           //Takes the Second Number
+           System.Console.WriteLine("Enter the Second Number");
+           var secondNumber = int.Parse(Console.ReadLine());
+
+           //Divides the the first number by the second number
+           var result = firstNumber % secondNumber;
+
+           //Print the division result
+           System.Console.WriteLine("The remainder is " + result);
+        }
+    }
+}
